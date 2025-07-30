30cb42355b12cc2498a1379473cf8fb8339fb9e7
Michael Lebowitz <mikelebowitz@icloud.com>
2025-07-29 21:20:44 -0400

    checkpoint: fix error from console
    Image: /Users/mikelebow...

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Enable CORS for development
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:3000'
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   if (req.method === 'OPTIONS') {
